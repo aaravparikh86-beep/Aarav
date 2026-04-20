33b3a3a5a9fbcebd2dc756b1095f2524d919a9ab
unknown <aaravparikh86-beep>
2026-04-20 16:16:24 +0530

    comment removed

--- factorial.py
+++ factorial.py
@@ -1,20 +1,10 @@
-# num = int(input("Enter a number whose factorial you want to remove: "))
-# num1 = num
-# while num > 0:
-#     if num < 1:
-#         print(num,"is the factorial of",num1)
-#     else:
-#         num = num1 * num
-#         num1-=1
-# else:
-#     print("Enter a number greater than 0")
     
 
-def fact(n):
+def fact(n): #n(n-1)! 5
     if n==0 or n ==1:
         return 1
     else:
-        return n*fact(n-1)
+        return n*fact(n-1) #5(5-1)!
     
 factorial = fact(5)
 print(factorial)
