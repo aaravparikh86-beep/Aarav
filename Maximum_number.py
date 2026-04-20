928d5cdbdbc3313b0618130254579234956ac4f0
unknown <aaravparikh86-beep>
2026-04-20 16:28:23 +0530

    Maximum of 3

---
+++ Maximum_number.py
@@ -0,0 +1,8 @@
+def greatest():
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number: "))
+    num3 = int(input("Enter your third number: "))
+    maximum = max(num1,num2,num3)
+    print("The maximum number is",maximum)
+
+greatest()
